f1994bef4510c50ee8a464d549761323741c65fa
Cody Burmeister <cody.burmeister1@gmail.com>
2017-09-29 23:54:32 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Basic Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Sriracha Burger');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Smoked Gouda Burger');
